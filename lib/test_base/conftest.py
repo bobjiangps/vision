e878e42e4e702e2b8f3e9a1ea94f5fcc1e948c84
bobjiangps <jbsv43@sina.com>
2021-10-28 17:39:23 +0800

    add more configurations

--- lib/test_base/conftest.py
+++ lib/test_base/conftest.py
@@ -4,16 +4,35 @@ from utils.selenium_utils import SeleniumUtils
 from lib.action.web import WebAction
 
 
-def pytest_configure():
+def pytest_configure(config):
     print("loading model...")
     setattr(pytest, "model", init_model())
+    print("model loaded...")
+    command_data = getattr(pytest, "command_data")
+    if hasattr(config, '_metadata'):
+        for item in ["Packages", "Plugins", "JAVA_HOME"]:
+            if item in config._metadata.keys():
+                del config._metadata[item]
+        config._metadata["Environment"] = command_data["environment"]
+        if command_data["browser"]:
+            config._metadata["Browser"] = command_data["browser"]
+        elif command_data["mobile_platform"]:
+            config._metadata["Mobile Platform"] = command_data["mobile_platform"]
+            config._metadata["Device"] = command_data["device"]
+
+
+# not use pytest_addoption, because it does not allow lower case parameter
+# def pytest_addoption(parser):
+#     parser.addoption("-E", action="store", required=False, default="QA", help="which environment to test")
+#     parser.addoption("-B", action="store", required=False, default="Chrome", help="which browser to test")
 
 
 @pytest.fixture(scope="function")
 def web():
     web_test = type('web_test', (), {})()
+    command_data = getattr(pytest, "command_data")
     url = ""
-    driver = SeleniumUtils.get_driver("Chrome")
+    driver = SeleniumUtils.get_driver(command_data["browser"])
     action = WebAction(driver, getattr(pytest, "model"))
     setattr(web_test, "_driver", driver)
     setattr(web_test, "action", action)
