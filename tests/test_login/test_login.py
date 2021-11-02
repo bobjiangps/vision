545be9122dfb2dc306b5a1e01968b175be51da6c
bobjiangps <jbsv43@sina.com>
2021-11-02 17:50:36 +0800

    update test script to debug base class

--- tests/test_login/test_login.py
+++ tests/test_login/test_login.py
@@ -1,13 +1,21 @@
-def test_cui_login(web):
+def test_cui_login_successful(web):
     """BVT-1: test login"""
     web.log.info("user login")
     web.static("Login ID | Login name").input("ANE54527")
     web.text_field("Password").input("123456")
     web.button("Login | Sign in").click()
+    web.button("Login | Sign in").wait_element_disappear()
+    web.static("Account Options").wait_text_visible()
     # validate the restart attribute below
-    import time
-    time.sleep(5)
     web.action.restart_browser()
     web.action.browse_page("https://www.stackoverflow.com")
     web.static("Log in").click()
-    time.sleep(5)
+
+
+def test_cui_login_fail(web):
+    """BVT-2: test login with wrong account"""
+    web.log.info("user login")
+    web.static("Login ID | Login name").input("unknown")
+    web.text_field("Password").input("123456")
+    web.button("Login | Sign in").click()
+    web.static("Invalid login name or password").wait_text_visible()
