abec1ed15eb14d205fb6db964d5df5dd352f757b
bobjiangps <jbsv43@sina.com>
2022-01-19 14:37:02 +0800

    update page object sample

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,17 +11,14 @@ class LoginPage(PageBase):
         self.account_option_static = platform.static("Account Options")
 
     def fill_account(self, account):
-        self.platform.log.info("start to fill account")
         self.login_static.input(account)
         return self
 
     def fill_password(self, pw):
-        self.platform.log.info("start to fill pw")
         self.password_field.input(pw)
         return self
 
     def click_login(self):
-        self.platform.log.info("start to click login button")
         self.login_btn.click()
         return self
 
@@ -30,6 +27,7 @@ class LoginPage(PageBase):
         self.fill_account(account).fill_password(pw).click_login()
         self.login_btn.wait_element_disappear()
         self.account_option_static.wait_text_visible()
+        # here can return another page object like Home()
 
     def login_site_temp(self, account, pw):
         self.platform.log.info("start to login")
