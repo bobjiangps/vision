000cae18af2421c5ff5b455b635b10e1751fa3db
bobjiangps <jbsv43@sina.com>
2022-02-24 11:01:06 +0800

    add new parameter for text field to set direction to match

--- lib/elements/text_field.py
+++ lib/elements/text_field.py
@@ -4,9 +4,10 @@ import re
 
 class TextField(ElementBase):
 
-    def __init__(self, keyword=None):
+    def __init__(self, keyword=None, direction=None):
         super().__init__()
         self.keyword = keyword
+        self.direction = direction
         self.element_type = "_".join(re.findall("[A-Z][^A-Z]*", self.__class__.__qualname__))
 
     def input(self, value):
