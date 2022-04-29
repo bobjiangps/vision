4e1b70fbce3874a593e1713659538fb3ac0f83b5
bobjiangps <jbsv43@sina.com>
2022-04-29 11:11:30 +0800

    udpate code to handle dropdown

--- lib/elements/dropdown.py
+++ lib/elements/dropdown.py
@@ -9,11 +9,12 @@ class Dropdown(ElementBase):
         self.direction = direction
         self.element_type = self.__class__.__mro__[-3].__qualname__
 
-    def select_item(self, name, *args, **kwargs):
+    def select_item(self, name, *args):
         dropdown = self.wait_element_visible()
         self.action_click(dropdown)
-        if kwargs.get("normal"):
-            self._action.common_option_non_ai_element(name).click()
+        item = self._action.common_option_legacy_element(name)
+        if item:
+            item.click()
         else:
             self.action_send_value(dropdown, name.lower())
             self.action_press_key(dropdown, "enter")
