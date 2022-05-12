6251801ea1cd23c1d0c8a55fefe4cab8b4d33ea0
bobjiangps <jbsv43@sina.com>
2022-05-12 16:25:12 +0800

    add new class to define icon

---
+++ lib/elements/icon.py
@@ -0,0 +1,17 @@
+from lib.elements.base import ElementBase
+
+
+class Icon(ElementBase):
+
+    def __init__(self, category, keyword=None):
+        super().__init__()
+        self.element_type = self.__class__.__mro__[-3].__qualname__
+        self.category = category
+        if not keyword:
+            self.beyond = False
+        else:
+            self.keyword = keyword
+
+    def click(self, *args):
+        icon = self.wait_element_visible()
+        self.action_click(icon)
